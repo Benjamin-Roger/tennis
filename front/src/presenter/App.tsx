71ee439b27a8b2e59886bbb37d09c84b0ce0515e
Benjamin Roger <benjamin.roger@polyconseil.fr>
2022-05-26 16:14:04 +0200

    [Front] Add front boilerplate

---
+++ front/src/presenter/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
